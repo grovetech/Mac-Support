4f9b0df1de6b6ebaaeefd5da8f3214de7556f109
Jon Brown <jon@grovetech.co>
2019-06-12 12:49:48 -0400

    Initial Commit

---
+++ Grove Support/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Grove Support
+//
+//  Created by Jon Brown on 6/12/19.
+//  Copyright © 2019 Jon Brown. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Grove Support/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Grove Support
+//
+//  Created by Jon Brown on 6/12/19.
+//  Copyright © 2019 Jon Brown. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
